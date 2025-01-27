e977a7c93e1df6f7f201ba97cc0069036bd67e92
dkAsCambodia <nitindamo50@gmail.com>
2025-01-27 17:31:41 +0700

    integrate xprizo deposit in pay2Rax InProgress

--- resources/views/payment-form/xpz/payintest.blade.php
+++ resources/views/payment-form/xpz/payintest.blade.php
@@ -8,14 +8,14 @@ $referenceNo = "GZTRN" . time() . (function ($length = 3) {
     }
     return $randomString;
 })();
-$apiUrl = "http://127.0.0.1:8000/api/xpz/deposit";
-// $apiUrl = "https://payment.implogix.com/api/xpz/deposit";
+// $apiUrl = "http://127.0.0.1:8000/api/xpz/deposit";
+$apiUrl = "https://payment.pay2rax.com/api/xpz/deposit";
 $data = [
     'merchant_code' => $_GET['merchant_code'], 
     'product_id' => '21',
     'referenceId' => $referenceNo, 
-    'callback_url' => 'http://127.0.0.1:8000/api/xpz/depositResponse',
-    // 'callback_url' => 'https://payment.implogix.com/api/s2p/payin/callbackURL',
+    // 'callback_url' => 'http://127.0.0.1:8000/api/xpz/depositResponse',
+    'callback_url' => 'https://payment.pay2rax.com/api/xpz/depositResponse',
     'Currency' => $_GET['Currency'],
     'amount' => $_GET['amount'], 
     'customer_name' => $_GET['card_holder_name'],    // account holder name 
