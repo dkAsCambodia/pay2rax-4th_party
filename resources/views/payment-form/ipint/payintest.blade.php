617a7cdceaca88cf7aa723f8e5726bcf469446f8
dkAsCambodia <nitindamo50@gmail.com>
2025-02-22 15:46:24 +0700

    integrate ipint crypto payment by using laravel Done

---
+++ resources/views/payment-form/ipint/payintest.blade.php
@@ -0,0 +1,29 @@
+<?php
+$referenceNo = "GZTRN" . time() . (function ($length = 3) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+})();
+$apiUrl = "http://127.0.0.1:8000/api/ipint/checkout";
+// $apiUrl = "https://payment.pay2rax.com/api/ipint/checkout";
+$data = [
+    'merchant_code' => $_GET['merchant_code'],
+    'product_id' => '20',
+    'referenceId' => $referenceNo, 
+    'callback_url' => 'http://127.0.0.1:8000/api/ip/depositResponse',
+    // 'callback_url' => 'https://payment.pay2rax.com/api/ip/depositResponse',
+    'Currency' =>  $_GET['currency'], 
+    'amount' => $_GET['amount'],   
+    'customer_email' => 'dk@gmail.com', 
+    'customer_name' => 'dk John Doe', 
+];
+$fullUrl = $apiUrl . '?' . http_build_query($data);
+?>
+<script>
+    window.location.href = '<?php echo $fullUrl; ?>';     
+</script>
+
